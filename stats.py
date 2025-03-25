e16627164bde779775c84557ddcc66e62fe0aa8e
emxrald <yasakudo@gmail.com>
2025-03-25 20:12:45 +0100

    refactor code; add documentation

--- stats.py
+++ stats.py
@@ -3,8 +3,8 @@ from pprint import pprint
 
 def get_word_count(content: str) -> int:
     """Return word count in the string."""
-    wc = len(content.split())
-    return wc
+    # split by whitespace
+    return len(content.split())
 
 
 def get_char_count(content: str) -> dict[str, int]:
@@ -21,18 +21,19 @@ def get_char_count(content: str) -> dict[str, int]:
 
 
 def sort_key(dict):
+    """Function used for sorting char count list returned by 'sort_char_count'."""
     return dict["count"]
 
 
 def sort_char_count(char_count: dict[str, int]) -> list[dict]:
-    output_list = []
+    """Return list of character count dictionaries osrted by count desc."""
+    output_list = [] # output list
     for item in char_count:
         ch = item
         count = char_count[item]
         output_dict = {"character": ch, "count": count}
         output_list.append(output_dict)
 
-    # pprint(output_list)
     # sort()  does sorting in place, it does not return sorted object.
     output_list.sort(key=sort_key, reverse=True)
 
